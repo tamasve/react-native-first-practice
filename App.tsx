a058727515e763fc7bf798db442651343765d63c
tamasve <145750552+tamasve@users.noreply.github.com>
2023-12-22 01:36:17 +0100

    styles

--- App.tsx
+++ App.tsx
@@ -5,10 +5,11 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
-    Button,
+  Button,
+  Pressable,
   Linking,
   ScrollView,
   StyleSheet,
@@ -19,21 +20,48 @@ import {
 
 function App(): React.JSX.Element {
 
+  const [name, setName] = useState<string>('Tamas');
+  const [session, setSession] = useState<object>({number: 1, title: "state"});
+
+  const buttonClick = () => {
+    name === "Tamas" ? setName("Bela") : setName("Tamas");
+    let number = session.number;
+    setSession({number: number + 1,
+                title: ['styles', 'days', 'years', 'decades'][(number + 1) % 4]});
+  }
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Hello Everyone!</Text>
-        <Text style={styles.text}>What's the problem?</Text>
-      <Button title="Push me" onPress={() => Linking.openURL('https://www.idokep.hu/idojaras/Budapest')}></Button>
+      
+      <Button title="Back to session #1" onPress={() => setSession({number: 1, title: "state"})}></Button>
+      
+      <Text style={styles.text}>Hello {name}!</Text>
+      
+      <Button title="What's the weather like?"
+            onPress={() => Linking.openURL('https://www.idokep.hu/idojaras/Budapest')}>
+      </Button>
+      
+      <Text style={styles.text}>This is session #{session.number}, it's about {session.title}</Text>
+      
+      <Pressable style={styles.button} onPress={buttonClick}>
+        <Text style={styles.buttontext}>Change name and session</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: '#eeee00',
-    flex: 1,
+    width: '90%',
+    height: '70%',
+    margin: '5%',
+    // flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#eeee00',
+    borderWidth: 10,
+    borderColor: 'magenta',
+    borderRadius: 30,
 },
   text: {
     fontSize: 32,
@@ -46,6 +74,18 @@ const styles = StyleSheet.create({
   scrolltext: {
     height: 500,
     backgroundColor: 'pink',
+  },
+  button: {
+    backgroundColor: 'green',
+    borderRadius: 20,
+    margin: 5,
+    padding: 5,
+    elevation: 13
+  },
+  buttontext: {
+    color: 'white',
+    fontSize: 20,
+    padding: 5
   }
 });
 
